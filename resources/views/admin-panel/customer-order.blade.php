8dfeecac97bbe5e7d4f2e6ca8f532dc54521b7fe
Amr Akram <amroaarsi@gmail.com>
2022-12-05 23:52:50 +0200

    new update

--- resources/views/admin-panel/customer-order.blade.php
+++ resources/views/admin-panel/customer-order.blade.php
@@ -86,8 +86,6 @@
             display: flex;
             flex-direction: column;
             justify-content: center;
-            align-items: center;
-            text-align: center;
             min-height: 100vh;
         }
     </style>
